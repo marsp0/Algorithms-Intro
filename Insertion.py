f1289468046f262fd998cc7ac24bfebc3c4393e9
Martin Spasov <Suburb4nFilth@gmail.com>
2016-11-10 15:31:12 +0200

    Added Notes file

--- Insertion.py
+++ Insertion.py
@@ -4,6 +4,15 @@
 	- its run time is n*log_2(n)
 	- it sorts the elements in place
 
+	Loop Inviariant of the insertion sort: At the start of each iteration of the for loop the sublist iterable[0:j-1] consists of the elements originally in the list,
+	but in a sorted order.
+
+	Three points of the loop invariant :
+
+	1. Initialization : before the starting of the loop, the iterable[0] is a list with 1 element and thus is sorted.
+	2. Maintanence : the loop invariant hold at the beginning of every iteration. The sequence iterable[0:j-1] is a sorted.
+	3. Termination : at this point the variable used to count the current index should be the same as the len of the iterable
+
 '''
 
 import random
@@ -12,8 +21,10 @@ import time
 def sort_by_insertion(iterable):
 	#constant guard
 	i = 1
-	#start iteratin over the sequence 
-	while (i < len(iterable)) :
+	len_to_reach = len(iterable)
+	#start iterating over the sequence 
+	# NOTE:  the while loop here is faster than the for loop
+	while (i < len_to_reach):
 		#start a second loop to sort the current element in the already sorted part of the sequence
 		j = i
 		while ((j > 0) and (iterable[j] < iterable[j-1])):
