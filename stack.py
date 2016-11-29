3817d3b1d6e63bf2be6372f1d1ed733b159142cc
SuburbanFilth <Suburb4nFilth@gmail.com>
2016-11-29 11:37:28 +0200

    added basic stack implementation

---
+++ stack.py
@@ -0,0 +1,28 @@
+class Stack(object):
+
+	def __init__(self):
+
+		self.top = Node()
+
+	def push(self, data):
+		new_top = Node(data)
+		new_top.next_node, self.top = self.top, new_top
+		
+	def pop(self):
+
+		to_pop, self.top = self.top, self.top.next_node
+	
+	def traverse(self):
+		pass
+
+
+
+class Node(object):
+
+	def __init__(self,data = None):
+
+		self.data = None
+		self.next_node = None
+
+	def insert(self,data):
+		pass
