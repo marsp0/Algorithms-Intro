42e97c3865123bd838d7d263dacd06ebcaab01ae
SuburbanFilth <Suburb4nFilth@gmail.com>
2016-12-06 22:46:35 +0200

    Added heap data structure and finished insertion (no analysis)

--- heap.py
+++ heap.py
@@ -1,13 +1,42 @@
-class Heap(object):
+class MinHeap(object):
 
 	def __init__(self):
 
-		pass
+		self.heap = []
 
 	def insert(self,element):
+		self.heap.append(element)
+		if len(self.heap) == 1:
+			return
+		current_index = len(self.heap) - 1
+		while True:
+			if current_index % 2 == 1 and current_index != 0:
+				if self.heap[current_index] < self.heap[(current_index-1)//2]:
+					self.heap[current_index], self.heap[(current_index-1)//2] = self.heap[(current_index-1)//2] , self.heap[current_index]
+					current_index = (current_index-1)//2
+				else:
+					break
+			elif current_index % 2 == 0 and current_index != 0:
+				if self.heap[current_index] < self.heap[(current_index-1)//2]:
+					self.heap[current_index], self.heap[(current_index-1)//2] = self.heap[(current_index-1)//2] , self.heap[current_index]
+					current_index = (current_index-2)//2
+				else:
+					break
+			else:
+				return
+		return
 
-		pass
 
 	def delete(self,element):
 
 		pass
+
+if __name__ == '__main__':
+
+
+	p = MinHeap()
+	for i in xrange(20, 1, -1):
+
+		p.insert(i)
+	
+	print p.heap
