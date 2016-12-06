cea3c31c5ac843a5d87e5669b5f17ecdfb9991f8
SuburbanFilth <Suburb4nFilth@gmail.com>
2016-12-06 22:02:42 +0200

    fixed spaces

--- binary_tree.py
+++ binary_tree.py
@@ -11,20 +11,16 @@
 
 class Tree(object):
 
-	def __init__(self):
-		
+	def __init__(self):		
 		self.root = Node()
 
 	def insert(self,data):
-
 		self.root.insert(data)
 
 	def delete(self,data):
-
 		self.root.delete(data)
 
 	def print_elements(self):
-
 		return self.root.print_element()
 
 class Node(object):
@@ -37,9 +33,7 @@ class Node(object):
 		self.right = None
 
 	def insert(self,data):
-
 		if self.data == None:
-
 			self.data = data
 		else:
 			if data < self.data:
@@ -55,35 +49,21 @@ class Node(object):
 						self.right.insert(data)
 
 	def delete(self,data):
-
-
 		if self.data == data:
-
 			if self.right == None and self.left == None:
-
 				if self.parent == None:
-
 					self.data = None
 				else:
-
 					if self.parent.left.data == data:
-
 						self.parent.left = None
 					else:
-
 						self.parent.right = None
 			elif self.right == None or self.left == None:
-
 				if self.right == None:
-
 					self.data , self.left = self.left.data , self.left.left
-				
 				else:
-
 					self.data, self.right = self.right.data , self.right.right
-
 			else:
-
 				counter = self.right
 				while True:
 					if counter.left == None:
@@ -92,7 +72,6 @@ class Node(object):
 						counter = counter.left
 				self.data = counter.data
 				counter.delete(counter.data)
-
 		else:
 			if data < self.data:
 				self.left.delete(data)
@@ -101,7 +80,6 @@ class Node(object):
 		
 
 	def search(self,data):
-
 		if self.data == data:
 			return self.data
 		else:
@@ -111,13 +89,10 @@ class Node(object):
 				return self.right.search(data)
 
 	def print_element(self):
-
 		if self.parent:
-
 			print self.data, 'is below %s' % (self.parent.data)
 		else:
 			print self.data
-
 		if self.right != None:
 			self.right.print_element()
 		if self.left != None:
