62fe05cb4d28b60e8e424767d5e2d417fd09f845
SuburbanFilth <Suburb4nFilth@gmail.com>
2016-12-11 18:58:28 +0200

    re-implemented the heap structure decoupling some of the code

--- heap.py
+++ heap.py
@@ -9,66 +9,84 @@
 import time
 import random
 
-class MinHeap(object):
+class Heap(object):
 
 	def __init__(self):
-
-		self.heap = []
+		''' a heap starting from the 1st element and trying to decouple the two functions
+			first child is at 2n
+			second child is at 2n + 1
+		'''
+		self.heap = [0]
+		self.size = 0
 
 	def insert(self,element):
+
 		self.heap.append(element)
-		if len(self.heap) == 1:
-			return
-		current_index = len(self.heap) - 1
-		while True:
-			if current_index % 2 == 1 and current_index != 0:
-				if self.heap[current_index] < self.heap[(current_index-1)//2]:
-					self.heap[current_index], self.heap[(current_index-1)//2] = self.heap[(current_index-1)//2] , self.heap[current_index]
-					current_index = (current_index-1)//2
-				else:
-					break
-			elif current_index % 2 == 0 and current_index != 0:
-				if self.heap[current_index] < self.heap[(current_index-1)//2]:
-					self.heap[current_index], self.heap[(current_index-1)//2] = self.heap[(current_index-1)//2] , self.heap[current_index]
-					current_index = (current_index-2)//2
-				else:
-					break
+		self.size += 1
+		self.bubble_up(self.size)
+		return True
+
+	def bubble_up(self,index):
+		#reach the top of the heap
+		while index // 2 > 0:
+			if self.heap[index] < self.heap[index//2]:
+				self.heap[index], self.heap[index//2] = self.heap[index//2], self.heap[index]
+			index = index // 2
+		return True
+
+	def bubble_down(self,index):
+		#NOTE : REFORMAT THE CODE HERE
+		while index*2 <= self.size:
+			min_index = self.min_child(index)
+			self.heap[index], self.heap[min_index] = self.heap[min_index], self.heap[index]
+			index = min_index
+		return True
+
+	def min_child(self,index):
+		if (index*2 + 1) <= self.size:
+			if self.heap[index*2] < self.heap[index*2 + 1]:
+				return index*2
 			else:
-				return
-		return
+				return index*2 + 1
+		else:
+			return index*2
 
 	def extract_min(self):
 
-		to_return = self.heap[0]
-		self.heap[0] = self.heap.pop(-1)
-		while True:
-			if self.heap[0] < self.heap[1] and self.heap[0] < self.heap[2]:
-				return to_return
-			else:
-				if self.heap[1] < self.heap[2]:
-					self.heap[0], self.heap[1] = self.heap[1], self.heap[0]
-				else:
-					self.heap[0], self.heap[2] = self.heap[2], self.heap[0]
+		to_return = self.heap[1]
+		self.size -= 1
+		if len(self.heap) == 2:
+			pass
+		else:
+			self.heap[1] = self.heap.pop(-1)
+			self.bubble_down(1)
+		return to_return
+
+	def __len__(self):
+
+		return self.size
+
 
 
 def heapsort(array):
 
-	p = MinHeap()
+	p = Heap()
 	new_one = []
 	for i in array:
 		p.insert(i)
 	before = time.time()
 	for index in xrange(len(array)):
-
 		new_one.append(p.extract_min())
 	after = time.time()
 	print after - before
 	return new_one
 
 if __name__ == '__main__':
+
 	array = []
+
 	for i in xrange(10):
 
-		array.append(random.randint(1,1000))
+		array.append(random.randint(1,10000))
 
 	print heapsort(array)
