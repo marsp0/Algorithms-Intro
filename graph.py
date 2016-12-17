a176a5e329bc8f771e5dd1beac3e306e7b3d3445
SuburbanFilth <Suburb4nFilth@gmail.com>
2016-12-17 17:55:21 +0200

    degree counter

--- graph.py
+++ graph.py
@@ -4,22 +4,25 @@ class Graph(object):
 
 		self._graph = {}
 		self._size = 0
-		self._degre = {}
+		self._degree = {}
 
 	def add(self,vert_1, vert_2=None):
 
 		if vert_2 != None
 			self._graph[vert_1].append(vert_2)
+			self._degree[vert_1] += 1
 		else:
 			self._graph[vert_1] = []
-			self._size -= 1
+			self._size += 1
+			self._degree[vert_1] = 1
 		return True
 
 	def remove(self,vert_1,vert_2=None):
 
 		if vert_2 != None:
 			self._graph[vert_1].remove(vert_2)
+			self._degree[vert_1] -= 1
 		else:
 			del self._graph[vert_1]
+			del self._degree[vert_1]
 			self._size -= 1
-	
