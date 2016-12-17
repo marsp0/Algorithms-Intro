857e0c70196882dd50901ac505e6c3ced046b9da
SuburbanFilth <Suburb4nFilth@gmail.com>
2016-12-17 17:15:18 +0200

    finished implementation of add and remove

--- graph.py
+++ graph.py
@@ -2,6 +2,24 @@ class Graph(object):
 
 	def __init__(self):
 
-		pass
+		self._graph = {}
+		self._size = 0
+		self._degre = {}
 
+	def add(self,vert_1, vert_2=None):
+
+		if vert_2 != None
+			self._graph[vert_1].append(vert_2)
+		else:
+			self._graph[vert_1] = []
+			self._size -= 1
+		return True
+
+	def remove(self,vert_1,vert_2=None):
+
+		if vert_2 != None:
+			self._graph[vert_1].remove(vert_2)
+		else:
+			del self._graph[vert_1]
+			self._size -= 1
 	
