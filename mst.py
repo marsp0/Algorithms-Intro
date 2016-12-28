67afe49e7db1644b5f396803a19a1f295745d4a1
SuburbanFilth <Suburb4nFilth@gmail.com>
2016-12-28 12:17:36 +0200

    adding MST

---
+++ mst.py
@@ -0,0 +1,11 @@
+def weight(function,vertex_1, vertex_2):
+
+	return function(vertex_1,vertex_2)
+
+def get_mst_kruskal(graph_vertices,graph_edges,root):
+	''' get a minimum spanning tree from an undirected connected graph
+		The graph is represented by two tuples containing the vertices and the edges
+	'''
+
+	pass
+
