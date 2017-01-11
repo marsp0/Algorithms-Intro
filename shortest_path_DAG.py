ded9df3b5883b31d3b7218e7da2576ff2bf6c991
SuburbanFilth <Suburb4nFilth@gmail.com>
2017-01-11 11:04:10 +0200

    started the SSSP for DAG

--- shortest_path_DAG.py
+++ shortest_path_DAG.py
@@ -13,3 +13,37 @@
 	O(V+E) because we use DFS 
 
 '''
+
+from collections import defaultdict
+from stack import Stack
+
+
+def topological_sort(vertices,edges):
+	graph = {}
+	visited = {}
+	stack = Stack()
+	for node in vertices:
+		graph[node] = []
+		visited[node] = False
+	for edge in edges:
+		graph[edge[0]].append(edge[1])
+	for root in graph:
+		if visited[root] == False:
+			topological_util(root,graph,visited,stack)
+	return stack
+
+
+def topological_util(node,graph,visited,stack):
+
+	visited[node] = True
+	for edge in graph[node]:
+		if visited[edge] == False:
+			topological_util(edge,graph,visited,stack)
+	stack.push(node)
+
+if __name__ == '__main__':
+
+	vertices = [1,2,3,4,5,6]
+	edges = [(1,2),(1,4),(2,3),(2,4),(2,6),(1,5),(5,3)]
+	stack = topological_sort(vertices,edges)
+	print stack.traverse()
