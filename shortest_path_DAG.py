2c8b445409cc7efcd0b6efcc470d1285af8bc13b
SuburbanFilth <Suburb4nFilth@gmail.com>
2017-01-11 12:11:30 +0200

    finished implementation

--- shortest_path_DAG.py
+++ shortest_path_DAG.py
@@ -16,6 +16,7 @@
 
 from collections import defaultdict
 from stack import Stack
+from bellman_ford import Vertex
 
 
 def topological_sort(vertices,edges):
@@ -41,9 +42,43 @@ def topological_util(node,graph,visited,stack):
 			topological_util(edge,graph,visited,stack)
 	stack.push(node)
 
+def SSSP_DAG(vertices,edges,root):
+	#sort the graph topologically
+	stack = topological_sort(vertices,edges).traverse()
+	#initialization
+	graph = {}
+	for node in vertices:
+		graph[node] = Vertex_1(node)
+		for edge in edges:
+			if edge[0] == node:
+				graph[node].edges.append((edge[1],edge[2]))
+
+	#NOTE : is there an easier way to get that root value to be 0 ?
+	for item in stack:
+		if item == root:
+			graph[root].current_min_weight = 0
+	for item in stack:
+		for edge in graph[item].edges:
+			if graph[edge[0]].current_min_weight > graph[item].current_min_weight + edge[1]:
+				graph[edge[0]].current_min_weight = graph[item].current_min_weight + edge[1]
+				graph[edge[0]].predecessor = item
+	return graph
+
+class Vertex_1(Vertex):
+
+	def __init__(self,name):
+		super(Vertex_1,self).__init__(name)
+		self.edges = []
+
+
 if __name__ == '__main__':
 
 	vertices = [1,2,3,4,5,6]
-	edges = [(1,2),(1,4),(2,3),(2,4),(2,6),(1,5),(5,3)]
-	stack = topological_sort(vertices,edges)
-	print stack.traverse()
+	edges = [(1,2,1),(1,4,2),(2,3,4),(2,4,4),(2,6,2),(1,5,3),(5,3,1)]
+	graph = SSSP_DAG(vertices,edges,1)
+	for item in graph.values():
+		print item.name
+		print item.current_min_weight
+		print item.predecessor
+		print item.edges
+		print
