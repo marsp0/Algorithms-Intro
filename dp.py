d9d2c61cd1ffa71f92b8765322b4915e8365d178
suburbanfilth <suburb4nfilth@gmail.com>
2017-04-06 12:33:25 +0300

    added LCS problem

--- dp.py
+++ dp.py
@@ -17,6 +17,12 @@
 		4. Recurse and memoize (top-down or bottom-up)
 		5. Solve the original problem
 
+	DP problems have two commong traits :
+
+	1. Optimal Substructure - the problems exhibit this trait if the optimal solution to it contains within it optimal solutions
+								to subproblems
+	2.
+
 '''
 
 import time
@@ -65,14 +71,41 @@ def fibonacci_iter(n):
 			memo_iter[i] = memo_iter[i-1] + memo_iter[i-2]
 	return memo_iter[n]
 
+def longest_common_sequence(string_a, string_b):
+	memo = {}
+	for i in xrange(string_a):
+		memo[-1,]
+	for i in xrange(len(string_a)):
+		for j in xrange(len(string_b)):
+			print memo
+			if i == 0 and j == 0:
+				if string_a[i] == string_b[j]:
+					memo[(i,j)] = 1
+				else:
+					memo[(i,j)] = 0
+			else:
+				if string_a[i] == string_b[j]:
+					print i-1,j-1
+					memo[(i,j)] = 1 + memo[i-1,j-1]
+				else:
+					first = (i-1,j)
+					second = (i,j-1)
+					if not first in memo:
+						first = (0,0)
+					if not second in memo:
+						second = (0,0)
+					memo[(i,j)] = max(memo[first], memo[second])
+	return memo
+
 if __name__ == '__main__':
 	#before_exp = time.time()
 	#print fibonacci_exp(38)
 	#after_exp = time.time()
 	#print 'the time for the exp function is ', after_exp - before_exp
 	#print
-	before_memo = time.time()
-	print fibonacci_iter(200)
-	after_memo = time.time()
-	print memo_iter
-	print 'the time after the memo function is ', after_memo - before_memo 
+	#before_memo = time.time()
+	#print fibonacci_iter(200)
+	#after_memo = time.time()
+	#print memo_iter
+	#print 'the time after the memo function is ', after_memo - before_memo 
+	print longest_common_sequence('abc','cba')
